ae7175c5145b65d88d087ca183ff5fd71c6f32ce
Antoine Geonet <antoinegeonet@MacBook-Air-de-Antoine-2.local>
2022-10-24 14:05:08 +0200

    ajout fonction chrono dans Chrono.h

---
+++ lib/Chrono.h
@@ -0,0 +1,14 @@
+#include <time.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void chrono(int delay) {
+    time_t start = time(NULL);
+
+    for (int i = 0; i < delay; ++i) {
+        time_t end = time(NULL);
+        int diff = difftime(end, start);
+        // printf("%d\n", diff);
+        sleep(1);
+    }
+}
