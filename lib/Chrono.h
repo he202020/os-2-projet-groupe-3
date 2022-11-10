79e3e685194d52f3a35421f3838733d08c658e78
Antoine Geonet <antoinegeonet@airdeantoine2.home>
2022-11-10 12:24:33 +0100

    Fonction chrono

--- lib/Chrono.h
+++ lib/Chrono.h
@@ -3,11 +3,12 @@
 #include <stdio.h>
 
 void chrono(int delay) {
-    time_t start = time(NULL);
+    // time_t start = time(NULL);
 
     for (int i = 0; i < delay; ++i) {
         time_t end = time(NULL);
-        int diff = difftime(end, start);
+        setCourseState(false);
+        // int diff = difftime(end, start);
         // printf("%d\n", diff);
         sleep(1);
     }
